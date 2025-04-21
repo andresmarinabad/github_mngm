acbf07c6f57d81510873960620a266ed1f8fa473
Andres M <andres.marin.abad+git@gmail.com>
2025-04-21 14:54:48 +0200

    Add telegram bot repo

--- repos.tf
+++ repos.tf
@@ -5,10 +5,27 @@ module "github_mngm" {
   visibility  = "public"
 }
 
+module "telegram_bot" {
+  source      = "./modules/github"
+  name        = "telegram_bot"
+  description = "Python bot for telegram with terraform form infra deploy"
+  visibility  = "public"
+}
+
 module "agapes" {
   source = "./modules/github"
   name        = "agapes"
   description = "HTML project to publish agapes list"
+  visibility  = "public"
+  providers = {
+    github = github.comu
+  }
+}
+
+module "grupos" {
+  source = "./modules/github"
+  name        = "grupos"
+  description = "HTML project to publish groups list"
   visibility  = "public"
   providers = {
     github = github.comu
