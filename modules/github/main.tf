96c647d2f7713fc7b32bc77f7441d618b4563e85
Andres M <andres.marin.abad+git@gmail.com>
2025-10-20 23:41:54 +0200

    feat: modul default main and dependabot yml

--- modules/github/main.tf
+++ modules/github/main.tf
@@ -32,3 +32,70 @@ resource "github_repository" "repository" {
   }
 
 }
+
+resource "github_branch" "main" {
+  repository = github_repository.repository.name
+  branch     = "main"
+}
+
+resource "github_branch_default" "default"{
+  repository = github_repository.repository.name
+  branch     = github_branch.main.branch
+}
+
+resource "github_issue_label" "labels" {
+  for_each = {
+    "patch" = { color = "0e8a16", description = "Increment patch" }
+    "minor" = { color = "fbca04", description = "Increment minor" }
+    "major" = { color = "d73a4a", description = "Increment major" }
+  }
+
+  repository  = github_repository.repository.name
+  name        = each.key
+  color       = each.value.color
+  description = each.value.description
+}
+
+resource "github_repository_dependabot_security_updates" "dependabot" {
+  repository  = github_repository.repository.name
+  enabled     = true
+}
+
+resource "github_repository_file" "dependabot" {
+  repository = github_repository.repository.name
+  file       = ".github/dependabot.yml"
+  content    = yamlencode({
+    version = 2
+    updates = [
+      {
+        package-ecosystem = "pip"
+        directory = "/"
+        schedule = {
+          interval = "monthly"
+        }
+        open-pull-requests-limit = 5
+        labels = ["patch"]
+      },
+      {
+        package-ecosystem = "terraform"
+        directory = "/"
+        schedule = {
+          interval = "monthly"
+        }
+        open-pull-requests-limit = 5
+        labels = ["patch"]
+      },
+      {
+        package-ecosystem = "github-actions"
+        directory = "/"
+        schedule = {
+          interval = "monthly"
+        }
+        open-pull-requests-limit = 5
+        labels = ["patch"]
+      }
+    ]
+  })
+  branch      = "main"
+  commit_message = "Add Dependabot configuration"
+}
